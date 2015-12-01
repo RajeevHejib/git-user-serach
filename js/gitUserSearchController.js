ff529fcbb1f4d71c9fb906ad164425fe6493c9c3
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-12-01 21:13:20 +0000

    Github API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -5,8 +5,7 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
 
   self.doSearch = function() {
     self.searchResult = searchResource.get(
-        { q: self.searchTerm,
-          access_token: "4eb03682b96f713d72de3399e09782e75c66279b"
+        { q: self.searchTerm
         }
       );
     };
