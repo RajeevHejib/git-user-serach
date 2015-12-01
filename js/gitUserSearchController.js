a49736b55c79dfc2564c75bad56e3a4815582086
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-12-01 16:37:09 +0000

    mocked call to Github API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,39 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
+  var searchResource = $resource('https://api.github.com/search/users');
   var self = this;
 
-  //
-  // self.searchResult = {
-  //   "items": [
-  //     {
-  //       "login": "tansaku",
-  //       "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-  //       "html_url": "https://github.com/tansaku"
-  //     },
-  //     {
-  //       "login": "stephenlloyd",
-  //       "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-  //       "html_url": "https://github.com/stephenlloyd"
-  //     }
-  //   ]
-  // };
-
-
-    self.doSearch = function() {
-      self.searchResult = {
-        items: [
-          {
-            "login": "tansaku",
-            "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-            "html_url": "https://github.com/tansaku"
-          },
-          {
-            "login": "stephenlloyd",
-            "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-            "html_url": "https://github.com/stephenlloyd"
-          }
-        ]
-      };
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+        { q: self.searchTerm,
+          access_token: "4eb03682b96f713d72de3399e09782e75c66279b"
+        }
+      );
     };
-
 }]);
